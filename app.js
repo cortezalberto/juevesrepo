9f65a04cb4b3017b3b07af3dd0130939139a192d
cortezalberto <cortezalberto@gmail.com>
2021-03-25 11:32:44 -0300

    Commit local primero

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const path = require('path');
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+
+
+app.listen(port, () => {
+    console.log('Example app listening on port '+ port);
+});
